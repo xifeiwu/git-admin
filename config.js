a58a9e45e0ed0eb4c6ba1a46666c22b47e5a77c0
wuxifei <xfwu@conviva.com>
2022-02-16 11:33:26 +0800

    add repo react/react-components-primer

--- config.js
+++ config.js
@@ -191,6 +191,7 @@ module.exports = {
         'react-app': props({
           desc: '通过create-react-app脚手架创建的一个react app demo'
         }),
+        'react-components-primer': props({desc: '一个用来展示react-component组件的框架'}),
         'components': props({
           desc: 'react自定义组件'
         }),
