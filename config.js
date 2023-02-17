c4bd46516fbb24eede90ed89c5fd8edcc8c0c8f6
xifei wu <xfwu@conviva.com>
2023-02-17 15:41:51 +0800

    add repo micro-frontend

--- config.js
+++ config.js
@@ -219,6 +219,9 @@ module.exports = {
     },
     react: {
       start: {
+        'micro-frontend': props({
+          desc: '微前端项目，集成了很多前端项目的入口'
+        }),
         'feature': props({
           desc: '展示react feature及常用vendor的使用'
         }),
